6b31011a94329c4396574da7e329cee33adbcc3e
t s g <tsg@tsg.home>
2021-11-19 15:35:33 +0000

    feat(site-flow): splashpage and homepage component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProductList from './components/pages/product-page'
-const SplashPage = <div>hello world</div>;
+import ProductList from './components/pages/product-page';
+import SplashPage from './components/pages/splash-page';
+import HomePage from './components/pages/home-page'
+
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
           <Route exact path="/" element={<SplashPage/>}/>
-          <Route exact path="/product" element={<ProductList/>}/>
+          <Route  path="/home" element={<HomePage/>}/>
+          <Route  path="/product" element={<ProductList/>}/>
         </Routes>
       </Router>
     </div>
